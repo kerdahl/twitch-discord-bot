8138973f30d26dc0247067ea8d7598198f21672f
Kyle Erdahl <kyle.m.erdahl@gmail.com>
2019-09-05 16:12:06 -0500

    Added express Heroku workaround maybe?

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
+require('dotenv').config();
+const express = require('express');
 const twitchbot = require('./twitchbot');
+
+const port = process.env.PORT;
+const app = express();
+app.listen(port, () => console.log(`Listening at port ${port}...`));
+app.get('/*', (request, response) => {
+  response.redirect('https://www.twitch.tv/inexpensivegamer/');
+});
 
 console.log('Loading bot...');
 twitchbot.connect();
